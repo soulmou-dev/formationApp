cd0695a379322b907d64c61b53fa54d684c88895
soulmou-dev <oulmou.salim@gmail.com>
2025-05-11 19:37:15 +0200

    ajout bootsrap jquery select2

--- importmap.php
+++ importmap.php
@@ -25,4 +25,24 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
+    'jquery' => [
+        'version' => '3.7.1',
+    ],
+    'bootstrap' => [
+        'version' => '5.3.6',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.6',
+        'type' => 'css',
+    ],
+    'select2' => [
+        'version' => '4.1.0-rc.0',
+    ],
+    'select2/dist/css/select2.min.css' => [
+        'version' => '4.1.0-rc.0',
+        'type' => 'css',
+    ],
 ];
